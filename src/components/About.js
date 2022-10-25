adb166c4289760fae13e891edb7fb6b82d9b7c6c
yallahbaba <jalal@anqatech.com>
2022-10-25 15:58:52 +0100

    Populated information to the About section

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,39 @@ import React from 'react'
 const About = () => {
   return (
     <div name='about' className='w-full my-32'>
-        <div className='max-w-[1240px] mx-auto'>
+        <div className='max-w-[1240px] mx-10'>
             <div className='text-center'>
-                <h2 className='text-5xl font-bold'>Trusted by developers across the world</h2>
-                <p className='text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque asperiores earum placeat veritatis dignissimos itaque.</p>
+                <h2 className='text-4xl font-bold'>
+                    75% of fleet managers rely on fleet management software
+                </h2>
+                <p className='text-3xl py-6 text-gray-500 text-left'>
+                    Managing a fleet of vehicles can be challenging, no matter the fleet size. 
+                    That's why over 75% of fleet managers rely on fleet management software or 
+                    vehicle telmatics to help support day-to-day operations
+                </p>
             </div>
 
             <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
                 <div className='border py-8 rounded-xl shadow-xl' >
-                    <p className='text-6xl font-bold text-indigo-600'>100%</p>
-                    <p className='text-gray-400 mt-2'>Completion</p>
+                    <p className='text-2xl font-bold text-indigo-600'>Fleet Tracking</p>
+                    <p className='text-gray-400 mt-2 px-2 text-left'>
+                        To ensure driver safety, fleet managers need to know exactly where 
+                        individual vehicles are at any given time
+                    </p>
                 </div>
                 <div  className='border py-8 rounded-xl shadow-xl' >
-                    <p className='text-6xl font-bold text-indigo-600'>24/7</p>
-                    <p className='text-gray-400 mt-2'>Delivery</p>
+                    <p className='text-2xl font-bold text-indigo-600'>Total Cost Reduction</p>
+                    <p className='text-gray-400 mt-2 px-2 text-left'>
+                        Whether it's fuel management or lowering operating costs, fleet managers 
+                        are expected to identify and weed out any unnecessary expenses
+                    </p>
                 </div>
                 <div className='border py-8 rounded-xl shadow-xl' >
-                    <p className='text-6xl font-bold text-indigo-600'>100K</p>
-                    <p className='text-gray-400 mt-2'>Transactions</p>
+                    <p className='text-2xl font-bold text-indigo-600'>Vehicle Utilization Forecast</p>
+                    <p className='text-gray-400 mt-2 px-2 text-left'>
+                        Correctly understanding and forecasting fleet vehicle utilization is paramount 
+                        for optimal resource allocation across geography
+                    </p>
                 </div>
             </div>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => {
         <div name="home" className="w-full h-screen">
             <video className="absolute object-cover h-full w-full -z-10" src={heroVideo} autoPlay loop muted/>
             <div className="w-full flex flex-col justify-center items-center text-white px-4 py-10 text-center">
-                <h1 className="text-3xl uppercase p-2">Fleet Data Management</h1>
-                <p className="text-lg text-red-100 py-4 w-1/2">
+                <h1 className="text-3xl uppercase p-2 md:text-5xl">Fleet Data Management</h1>
+                <p className="text-xl text-gray-400 py-4 w-1/2 md:text-3xl">
                     Anqatech uses machine learning and modern API tools to optimize
                     vehicle fleet management. The fleet data is fed into our model and
                     outputs realtime decisions to implement cost savings and resource 
