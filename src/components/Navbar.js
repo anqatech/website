32bae42018b75dde6391116cf9f804f1206a48fc
yallahbaba <jalal@anqatech.com>
2022-10-25 17:50:17 +0100

    Started to populate the Pricing section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='w-screen h-[80px] z-10 bg-orange-50 sticky top-0 drop-shadow-2xl'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
-        <div className='flex items-center'>
+        <div className='flex justify-between items-center w-full'>
           <h1 className='text-3xl font-bold mr-4 sm:text-4xl uppercase'>Anqatech</h1>
           <ul className='hidden md:flex'>
             <li className='cursor-pointer'><Link to="home" smooth={true} offset={-100} duration={500}>Home</Link></li>
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -24,7 +24,7 @@ const Pricing = () => {
           <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
             <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard</span>
             <div>
-              <p className='text-6xl font-bold py-4 flex'>$49<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+              <p className='text-6xl font-bold py-4 flex'>$699<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
             </div>
             <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
             <div className='text-2xl'>
@@ -39,7 +39,7 @@ const Pricing = () => {
           <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
             <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Premium</span>
             <div>
-              <p className='text-6xl font-bold py-4 flex'>$99<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+              <p className='text-6xl font-bold py-4 flex'>$999<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
             </div>
             <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
             <div className='text-2xl'>
